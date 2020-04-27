66324747bc8dbb625182df2597935e132d4ea981
Kushal Raj Gupta <kushalkrg@hotmail.com>
2020-04-27 13:17:14 +0530

    Create and setup bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -14,12 +14,53 @@ def list_bucksts():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
+
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
     "List objects of a bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure S3 bucket"
+
+    s3_bucket = s3.create_bucket(Bucket=bucket)
+    session.region_name
+    session.region_name
+    s3_bucket.upload_file('index.html','index.html',ExtraArgs={'ContentType':'text/html'})
+    policy="""
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": [
+                    "s3:GetObject"
+                ],
+                "Resource": [
+                    "arn:aws:s3:::%s/*"
+                ]
+            }
+        ]
+    }
+    """ % s3_bucket.name
+    policy=policy.strip()
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+            'ErrorDocument': {
+                'Key': 'error.html'
+            },
+            'IndexDocument': {
+                'Suffix': 'index.html'
+            }})
+    return
 
 if __name__ == '__main__':
     cli()
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Webotron is a script to sync a local dir to a S3 bucket
 ### Features
 - List buckets
 - List all objects of a bucket
+- Create  and configure new bucket
